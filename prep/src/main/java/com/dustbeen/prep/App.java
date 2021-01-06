ad5ac203e7df418c1ade903610548f68acf72d55
sachin.sharma <sachin.sharma@highradius.com>
2021-01-06 20:18:34 +0530

    first commit

---
+++ prep/src/main/java/com/dustbeen/prep/App.java
@@ -0,0 +1,13 @@
+package com.dustbeen.prep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
